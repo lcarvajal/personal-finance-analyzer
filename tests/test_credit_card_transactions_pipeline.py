01704c901a23b6660f0300375a613c703a86d757
Lukas Carvajal <lukascarvajal@gmail.com>
2024-05-14 16:11:12 +0200

    Update filepath for mock data

--- tests/test_credit_card_transactions_pipeline.py
+++ tests/test_credit_card_transactions_pipeline.py
@@ -7,8 +7,8 @@ from accounting import credit_card_transactions_pipeline as cc_pipeline
 
 class TestCreditCardTransactionsPipeline(unittest.TestCase):
     def setUp(self):
-        self.extracted_transactions = pd.read_csv('mock_data/valid_capital_one_transactions.csv', encoding='latin-1')
-        self.invalid_transactions_file_name = 'mock_data/invalid_capital_one_transactions.csv'
+        self.extracted_transactions = pd.read_csv('tests/mock_data/valid_capital_one_transactions.csv', encoding='latin-1')
+        self.invalid_transactions_file_name = 'tests/mock_data/invalid_capital_one_transactions.csv'
 
 
     def test_unexpected_csv_format(self):
