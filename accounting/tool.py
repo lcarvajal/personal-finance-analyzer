875f631a71bc1a52aa89fcc5263f55aa2f884ef0
Lukas Carvajal <lukascarvajal@gmail.com>
2024-05-16 14:59:36 +0200

    Add test to ensure no dataframes with invalid columns are loaded into transaction history

--- accounting/tool.py
+++ accounting/tool.py
@@ -5,12 +5,10 @@ import accounting.constant as c
 def send_to_trash(files):
     """Moves files to trash."""
     for file in files:
-        file_path = c.TEMP_DIRECTORY_PATH + file
-
         # Check if the file exists before attempting to delete
-        if os.path.exists(file_path):
+        if os.path.exists(file):
             # Move the file to the trash
-            send2trash.send2trash(file_path)
-            print(f"File '{file_path}' moved to trash successfully.")
+            send2trash.send2trash(file)
+            print(f"File '{file}' moved to trash successfully.")
         else:
-            print(f"File '{file_path}' does not exist.")
+            print(f"File '{file}' does not exist.")
--- tests/test_credit_card_transactions_pipeline.py
+++ tests/test_credit_card_transactions_pipeline.py
@@ -4,21 +4,33 @@ from pandera.errors import SchemaErrors
 
 import accounting.constant as c
 from accounting.pipelines.credit_card_transactions_pipeline import CreditCardTransactionsPipeline
+import accounting.tool as tool
 
 
 class TestCreditCardTransactionsPipeline(unittest.TestCase):
     def setUp(self):
         self.credit_card_transactions_pipeline = CreditCardTransactionsPipeline([])
-        # transaction_file_paths='tests/mock_data/valid_capital_one_transactions.csv'
+        self.invalid_transactions_file_name = 'tests/mock_data/invalid_capital_one_transactions.csv'
+        self.temp_file_paths = []
 
 
-    def test_unexpected_csv_format(self):
-        invalid_transactions_file_name = 'tests/mock_data/invalid_capital_one_transactions.csv'
-        df = self.credit_card_transactions_pipeline.extract_capital_one_transactions(file_path=invalid_transactions_file_name)
+    def test_invalid_transactions_csv_format(self):
+        df = self.credit_card_transactions_pipeline.extract_capital_one_transactions(file_path=self.invalid_transactions_file_name)
 
         with self.assertRaises(SchemaErrors):
             self.credit_card_transactions_pipeline.clean_capital_one_transactions(df)
-            
+    
+    def test_invalid_column_loaded_to_transactions_history(self):
+        df = pd.read_csv(self.invalid_transactions_file_name, encoding='latin-1')
+
+        temp_file_path = 'tests/mock_data/invalid_column_in_transactions.csv'
+        self.temp_file_paths.append(temp_file_path)
+
+        with self.assertRaises(SchemaErrors):
+            self.credit_card_transactions_pipeline.load_transactions(df, temp_file_path)
+
+    def tearDown(self):
+        tool.send_to_trash(self.temp_file_paths)
 
 if __name__ == '__main__':
     unittest.main()
